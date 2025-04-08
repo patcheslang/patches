c64d8919a280f8e0261865cae0a7a8c9fa7f2bc9
wikitopian <parrott.matt@protonmail.com>
2025-04-08 11:42:45 +0000

    init

---
+++ scripts/patches.js
@@ -0,0 +1,7 @@
+export default class Patches {
+	constructor() {
+		console.log("Patches loaded");
+	}
+}
+
+const patches = new Patches();
---
+++ web.js
@@ -0,0 +1,25 @@
+import express from "express";
+
+export default class PatchesWeb {
+	constructor(port = 3000) {
+		const app = express();
+
+		app.set("view engine", "ejs");
+
+		app.use("/favicon.ico", express.static("media/favicon.ico"));
+		app.use("/manifest.json", express.static("media/manifest.json"));
+
+		app.use("/media", express.static("media"));
+		app.use("/scripts", express.static("scripts"));
+		app.use("/styles", express.static("styles"));
+
+		app.get("/", (req, res) => res.render("index"));
+
+		app.all(/(.*)/, (_req, res) => res.sendStatus(404));
+
+		app.listen(port);
+	}
+}
+
+const port = Number(process.env.PORT ?? "3000");
+const patchesWeb = new PatchesWeb(port);
