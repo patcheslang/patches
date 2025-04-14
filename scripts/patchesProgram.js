ac1dde586aab41e9a22546253929d047a001d7dc
wikitopian <parrott.matt@protonmail.com>
2025-04-14 03:20:28 +0000

    add __STRING type

--- scripts/patchesProgram.js
+++ scripts/patchesProgram.js
@@ -12,6 +12,45 @@ export default class PatchesProgram {
 		this.doBootstrap();
 	}
 
+	add(intReps) {
+		for (const [name, table] of intReps.tableDefs.entries()) {
+			this.tableDefs.set(name, table);
+		}
+
+		this.prepops.push(...intReps.prepops);
+		this.instructions.push(...intReps.instructions);
+	}
+
+	compile() {
+		console.log("Compiling patches program...");
+		const mem = new DataView(this.memory.buffer);
+
+		let m = Number(this.UINT64_ADDRESS); // make room for instructions pointers
+		for (const [name, table] of this.tableDefs.entries()) {
+			table.address = m;
+			for (const tableMem of table.getTableMem()) {
+				mem.setBigUint64(m * 8, tableMem, true); // 8 bytes = 64 bits
+				m++;
+			}
+		}
+
+		for (const prepop of this.prepops) {
+			console.log("Prepop:", prepop);
+		}
+
+		for (const instruction of this.instructions) {
+			console.log("Instruction:", instruction);
+		}
+	}
+
+	run() {
+		console.log("Running patches program...");
+	}
+
+	getMemory() {
+		return this.memory;
+	}
+
 	doBootstrap() {
 		const __UINT64 = new Table(0n);
 		__UINT64.addField({
@@ -51,44 +90,20 @@ export default class PatchesProgram {
 		});
 
 		this.tableDefs.set("__UINT64", __UINT64);
-	}
 
-	add(intReps) {
-		for (const [name, table] of intReps.tableDefs.entries()) {
-			this.tableDefs.set(name, table);
-		}
+		const __STRING = new Table(this.UINT64_ADDRESS);
 
-		this.prepops.push(...intReps.prepops);
-		this.instructions.push(...intReps.instructions);
-	}
+		__STRING.addField({
+			primary: true,
+			autoIncrement: true,
+			nullable: false,
+			type: this.UINT64_ADDRESS,
+		});
 
-	compile() {
-		console.log("Compiling patches program...");
-		const mem = new DataView(this.memory.buffer);
+		__STRING.addField({
+			type: this.UINT64_ADDRESS,
+		});
 
-		let m = Number(this.UINT64_ADDRESS); // make room for instructions pointers
-		for (const [name, table] of this.tableDefs.entries()) {
-			table.address = m;
-			for (const tableMem of table.getTableMem()) {
-				mem.setBigUint64(m * 8, tableMem, true); // 8 bytes = 64 bits
-				m++;
-			}
-		}
-
-		for (const prepop of this.prepops) {
-			console.log("Prepop:", prepop);
-		}
-
-		for (const instruction of this.instructions) {
-			console.log("Instruction:", instruction);
-		}
-	}
-
-	run() {
-		console.log("Running patches program...");
-	}
-
-	getMemory() {
-		return this.memory;
+		this.tableDefs.set("__STRING", __STRING);
 	}
 }
